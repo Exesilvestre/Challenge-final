ecda4d44972dc4dde7483ab6fd758684d52aaba7
Exesilvestre <exesilvestre@gmail.com>
2024-12-19 17:54:51 -0300

    last changes

--- backend/prepare_vector_db.py
+++ backend/prepare_vector_db.py
@@ -2,17 +2,13 @@ import os
 import glob
 import time
 from dotenv import load_dotenv
-from langchain_cohere import CohereEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.vectorstores import Chroma
 from langchain.schema import Document
 from pypdf import PdfReader
 from utils.chroma_config import chroma_config
 
 def create_chunk_embedding():
     load_dotenv()
-
-    # Asegúrate de que la variable de entorno COHERE_API_KEY esté definida en tu archivo .env
     cohere_api_key = os.getenv("COHERE_API_KEY")
     if not cohere_api_key:
         raise ValueError("Falta la clave de API de Cohere en las variables de entorno.")
@@ -20,13 +16,12 @@ def create_chunk_embedding():
     # Ruta al directorio que contiene los archivos PDF
     pdf_directory = './data'
 
-    # Inicializa Chroma vector store con el directorio dado y el nombre de colección
+    # Inicializa Chroma vector store
     vectorstore = chroma_config()
 
     # Crea el separador de texto (text splitter)
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=200)
 
-    # Total de caracteres procesados
     total_characters = 0
 
     # Itera sobre todos los archivos PDF en el directorio y sus subdirectorios
@@ -62,8 +57,8 @@ def create_chunk_embedding():
             # Añade al vector store
             vectorstore.add_documents([doc])
 
-            # Espera 1 segundo entre cada llamada a la API (ajustar según sea necesario)
-            time.sleep(5)  # 1 segundo de espera entre llamadas
+            # Espera 5 segundos entre cada llamada a la API
+            time.sleep(5)  
 
     # Imprime el total de caracteres procesados
     print(f"Total de caracteres procesados: {total_characters}")
